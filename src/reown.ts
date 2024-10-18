8b22db56adb17f388137628e4f9ba6f881f5da48
Alex Lazar <alex2012ro@gmail.com>
2024-10-19 01:34:39 +0300

    use reown to connect to wallet and log address (#1)

---
+++ src/reown.ts
@@ -0,0 +1,26 @@
+import { createAppKit } from '@reown/appkit';
+import { mainnet } from '@reown/appkit/networks';
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
+
+const projectId = 'f980d1e8ac5f60d237a134a726ee17cd';
+
+export const networks = [mainnet];
+
+const wagmiAdapter = new WagmiAdapter({
+	projectId,
+	networks
+});
+
+const metadata = {
+	name: 'Eventus',
+	description: 'Simple RSVP for Crypto Events',
+	url: 'https://example.com', // origin must match your domain & subdomain
+	icons: ['https://avatars.githubusercontent.com/u/179229932']
+};
+
+export const modal = createAppKit({
+	adapters: [wagmiAdapter],
+	networks: [mainnet],
+	metadata,
+	projectId
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { modal } from '../reown';
 </script>
 
 <svelte:head>
@@ -10,22 +8,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<button on:click={() => console.log(modal.getAddress())}>Log Address</button>
 </section>
 
 <style>
@@ -37,23 +20,13 @@
 		flex: 0.6;
 	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	button {
+		padding: 1rem;
+		background-color: #1a1a1a;
+		color: white;
+		border: none;
+		border-radius: 0.5rem;
+		cursor: pointer;
 	}
 </style>
+
