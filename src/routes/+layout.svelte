744aa55c479cb0712e27af6f9cd4d2be7876be47
lazaralex98 <alex2012ro@gmail.com>
2024-10-19 04:21:28 +0300

    style page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <div class="app">
 	<Header />
 
-	<main>
+	<main class="mx-auto max-w-7xl sm:px-6 lg:px-8 py-10">
 		<slot />
 	</main>
 
@@ -14,4 +14,3 @@
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 	</footer>
 </div>
-
