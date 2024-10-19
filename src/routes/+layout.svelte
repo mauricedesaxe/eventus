3fb6c293d898c2d9012772a3067b9ebedc85920e
Alex Lazar <alex2012ro@gmail.com>
2024-10-19 17:08:04 +0300

    add simple ui for event details page (#8)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,7 @@
 <div class="app">
 	<Header />
 
-	<main class="mx-auto max-w-7xl sm:px-6 lg:px-8 py-10">
-		<slot />
-	</main>
+	<slot />
 
 	<footer class="bg-gray-100 p-4 text-center">
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
