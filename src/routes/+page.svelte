ba22b563ba12d408c766cf57605bfe303f8699fd
lazaralex98 <alex2012ro@gmail.com>
2024-10-19 04:21:40 +0300

    add events table
    with fake constant data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,139 @@
+<script>
+	const events = [
+		{
+			slug: 'secureum-istanbul-2024',
+			name: 'Secureum Istanbul',
+			date: '2024-11-03',
+			summary: 'Secureum Istanbul is a conference about blockchain security.',
+			isRSVPed: true
+		},
+		{
+			slug: 'ethglobal-istanbul-2024',
+			name: 'ETHGlobal Istanbul',
+			date: '2024-11-01',
+			summary: 'A hackathon and conference for Ethereum developers and enthusiasts.',
+			isRSVPed: false
+		},
+		{
+			slug: 'devcon-bogota-2025',
+			name: 'DevCon Bogotá',
+			date: '2025-02-11',
+			summary: 'The annual Ethereum developers conference, held in Bogotá, Colombia.',
+			isRSVPed: false
+		},
+		{
+			slug: 'nft-paris-2025',
+			name: 'NFT Paris',
+			date: '2025-03-23',
+			summary:
+				"Europe's largest NFT and Web3 event, bringing together artists, collectors, and innovators.",
+			isRSVPed: false
+		},
+		{
+			slug: 'ethdenver-2025',
+			name: 'ETHDenver',
+			date: '2025-01-29',
+			summary: "The world's largest Web3 #BUIDLathon and innovation festival.",
+			isRSVPed: false
+		}
+	];
+</script>
+
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="summary" content="Svelte demo app" />
 </svelte:head>
 
-
+<div class="px-4 sm:px-6 lg:px-8">
+	<div class="sm:flex sm:items-center">
+		<div class="sm:flex-auto">
+			<h1 class="text-base font-semibold leading-6 text-gray-900">Events</h1>
+			<p class="mt-2 text-sm text-gray-700">
+				RSVP to any of the upcoming events below using your wallet (you must connect your wallet to
+				Eventus to RSVP, address read-only).
+			</p>
+		</div>
+	</div>
+	<div class="-mx-4 mt-10 ring-1 ring-gray-300 sm:mx-0 sm:rounded-lg">
+		<table class="min-w-full divide-y divide-gray-300">
+			<thead>
+				<tr>
+					<th
+						scope="col"
+						class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+					>
+						Event</th
+					>
+					<th
+						scope="col"
+						class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+					>
+						Date</th
+					>
+					<th
+						scope="col"
+						class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+					>
+						Countdown</th
+					>
+					<th
+						scope="col"
+						class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+					>
+						Summary</th
+					>
+					<th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+						<span class="sr-only">RSVP</span>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each events as event}
+					<tr>
+						<td class="relative py-4 pl-4 pr-3 text-sm sm:pl-6">
+							<div class="font-medium text-gray-900">{event.name}</div>
+							<div class="mt-1 flex flex-col text-gray-500 sm:block lg:hidden">
+								<span>{event.date}</span>
+								<span class="hidden sm:inline">·</span>
+								<span>{event.summary}</span>
+							</div>
+						</td>
+						<td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell">
+							{new Date(event.date).toLocaleDateString('en-US', {
+								weekday: 'short',
+								year: 'numeric',
+								month: 'short',
+								day: 'numeric'
+							})}
+						</td>
+						<td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell">
+							{#if new Date(event.date) > new Date()}
+								{@const timeLeft = new Date(event.date).getTime() - new Date().getTime()}
+								{@const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))}
+								{@const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}
+								{days}d {hours}h
+							{:else if new Date(event.date) < new Date()}
+								Event has passed
+							{:else if new Date(event.date) === new Date()}
+								Event starts today
+							{:else}
+								Coming soon
+							{/if}
+						</td>
+						<td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell">
+							{event.summary}
+						</td>
+						<td class="relative py-3.5 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+							<a
+								href={`/event/${event.slug}`}
+								class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
+							>
+								Details<span class="sr-only"> about {event.name}</span></a
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</div>
