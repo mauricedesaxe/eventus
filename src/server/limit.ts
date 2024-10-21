9f58b763a1e6e4b3c1fb6e441b952b9e42595536
lazaralex98 <alex2012ro@gmail.com>
2024-10-21 04:27:20 +0300

    add "ignore rate limit" in dev mode

--- src/server/limit.ts
+++ src/server/limit.ts
@@ -3,6 +3,7 @@ import client from './redis';
 const MINUTE = 60 * 1000;
 const MAX_REQUESTS = 5;
 const WINDOW_MS = 60 * MINUTE;
+const IS_DEV_ENV = process.env.NODE_ENV === 'development';
 
 type RateLimitInput = {
 	request: Request;
@@ -17,6 +18,10 @@ export async function isRateLimited({
 	maxReq = MAX_REQUESTS,
 	windowMs = WINDOW_MS
 }: RateLimitInput): Promise<boolean> {
+	if (IS_DEV_ENV) {
+		return false;
+	}
+
 	const ip =
 		request.headers.get('x-forwarded-for') ||
 		request.headers.get('cf-connecting-ip') ||
