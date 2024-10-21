491dffe271409f78ce1a129e0e34314e1b0953e7
lazaralex98 <alex2012ro@gmail.com>
2024-10-21 06:13:07 +0300

    handle http errors

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, message }) => {
+				// Ignore 404 errors for the root path during prerendering
+				if (path === '/' && message.includes('404')) {
+					return;
+				}
+				// Throw other errors
+				throw new Error(message);
+			}
+		}
 	}
 };
 
