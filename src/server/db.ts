901b8998740f56c42feb6c34296a18929a46d6ca
lazaralex98 <alex2012ro@gmail.com>
2024-10-21 05:08:07 +0300

    keep related code together

--- src/server/db.ts
+++ src/server/db.ts
@@ -10,7 +10,6 @@ await sql`CREATE TABLE IF NOT EXISTS challenges (
 	CONSTRAINT unique_address UNIQUE (address),
 	CONSTRAINT expires_at_check CHECK (expires_at > NOW())
 )`;
-
 await sql`CREATE INDEX IF NOT EXISTS idx_challenges_address ON challenges (address)`;
 
 await sql`CREATE TABLE IF NOT EXISTS rsvps (
@@ -18,7 +17,6 @@ await sql`CREATE TABLE IF NOT EXISTS rsvps (
 	event_slug TEXT,
 	PRIMARY KEY (address, event_slug)
 )`;
-
 await sql`CREATE INDEX IF NOT EXISTS idx_rsvps_address ON rsvps (address)`;
 await sql`CREATE INDEX IF NOT EXISTS idx_rsvps_event_slug ON rsvps (event_slug)`;
 await sql`CREATE INDEX IF NOT EXISTS idx_rsvps_address_event_slug ON rsvps (address, event_slug)`;
