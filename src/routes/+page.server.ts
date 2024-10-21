08b3dd73fefa8ea319503e0f5484043986925fcd
lazaralex98 <alex2012ro@gmail.com>
2024-10-21 15:54:08 +0300

    be more imformative when upcoming events fail to load

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,23 +3,30 @@ import type { PageServerLoad } from './$types';
 import type { Event } from '$lib/types';
 
 export const load: PageServerLoad = async () => {
-	const events = await sql`
-        SELECT slug, name, date, summary, description, image
-        FROM events
-        ORDER BY date ASC
-    `;
-	if (!events) {
-		return {
-			error: 'Failed to fetch events'
-		};
-	}
-	if (events.length === 0) {
-		return {
-			error: 'No events found'
-		};
-	}
+	try {
+		const events = await sql`
+			SELECT slug, name, date, summary, description, image
+			FROM events
+			ORDER BY date ASC
+		`;
 
-	return {
-		events: events as unknown as Event[]
-	};
+		if (!events) {
+			return {
+				error: 'Failed to fetch events'
+			};
+		}
+		if (events.length === 0) {
+			return {
+				error: 'No events found'
+			};
+		}
+
+		return {
+			events: events as unknown as Event[]
+		};
+	} catch (error) {
+		return {
+			error: `An error occurred while fetching events: ${error}`
+		};
+	}
 };
